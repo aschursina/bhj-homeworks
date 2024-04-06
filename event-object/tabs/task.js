0d216ff78839c0d462fc7fd715bec4fcddb03ccc
aschursina <55243551+aschursina@users.noreply.github.com>
2024-04-06 20:19:51 +0300

    Add files via upload

---
+++ event-object/tabs/task.js
@@ -0,0 +1,22 @@
+let arrTabs = Array.from(document.querySelectorAll(".tabs"));
+
+arrTabs.forEach((tabs) => {
+  const arrItems = Array.from(tabs.querySelectorAll(".tab"));
+  const arrContent = Array.from(tabs.querySelectorAll(".tab__content"));
+  
+  arrItems.forEach((tabItem, tabIndex) => {
+    let  changeTab = function(event)  {
+      for (let i = 0; i < arrItems.length; i++) {
+        if (i === tabIndex) {
+          arrItems[i].classList.add("tab_active");
+          arrContent[i].classList.add("tab__content_active");
+        } else {
+          arrItems[i].classList.remove("tab_active");
+          arrContent[i].classList.remove("tab__content_active");
+        }
+      }
+    }
+    
+    tabItem.addEventListener("click", changeTab);
+  });
+});
