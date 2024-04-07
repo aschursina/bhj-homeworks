c22d6fd4d40ab03cb7968e7294712237c6762d6b
aschursina <55243551+aschursina@users.noreply.github.com>
2024-04-07 16:52:41 +0300

    fix event

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -4,22 +4,19 @@ let arrLink = Array.from(document.querySelectorAll(".dropdown__link"));
 arrDropdown.forEach((dropItem) => {
   let arrList = dropItem.querySelector(".dropdown__list");
   let arrValue = dropItem.querySelector(".dropdown__value");
-  let changeStatus = function(event) {
+  arrValue.addEventListener("click", event => {
     arrList.classList.toggle("dropdown__list_active");
-  }
-  
-  arrValue.addEventListener("click", changeStatus);
+  });
 
   arrLink.forEach((link) => {
-    let changeLink = function(event) {
+    link.addEventListener("click", event => {
       event.preventDefault();
-      arrValue.textContent = this.textContent;
+      arrValue.textContent = event.target.textContent;
       arrList.classList.remove("dropdown__list_active");
-    }
-    
-  link.addEventListener("click", changeLink);
+    });
   });
-})
+});
+
 
 
 
