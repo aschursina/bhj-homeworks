a5807cf2a9817e4a0d0bc745763ef9462538cfb4
aschursina <55243551+aschursina@users.noreply.github.com>
2024-04-07 22:45:21 +0300

    part result

---
+++ dom/ads/task.js
@@ -0,0 +1,20 @@
+let arr = Array.from(document.querySelectorAll(".rotator"));
+
+arr.forEach((e) => {
+ let arrCase = Array.from(e.querySelectorAll(".rotator__case"));
+ let currentRotator = arrCase.length - 1;
+ 
+ let change = () => {
+  if(currentRotator === arrCase.length - 1) {
+    currentRotator = 0;
+    arrCase[currentRotator].classList.remove("rotator__case_active")
+  }
+  currentRotator++;
+  arrCase[currentRotator].classList.add("rotator__case_active");
+ }
+
+ setInterval(change, 2000);
+});
+
+
+
