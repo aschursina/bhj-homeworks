1c997aa4e0c4713c2b5ff0bb4bbb82654bc3fefd
aschursina <55243551+aschursina@users.noreply.github.com>
2024-04-13 23:22:48 +0300

    fix inner

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -7,10 +7,9 @@ taskInput.addEventListener("keydown", (event) => {
   if(event.key === "Enter") {
     let newTask = document.createElement("div");
     newTask.classList.add("task");
-    newTask.innerHTML( 
+    newTask.innerHTML= 
       `<div class="task__title">${taskInput.value}</div>
-       <a href="#" class="task__remove">&times;</a>`
-    );
+       <a href="#" class="task__remove">&times;</a>`;
     tasks.appendChild(newTask);
     console.log(newTask)
   }
