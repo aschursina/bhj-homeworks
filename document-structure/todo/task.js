58b03f52164d699f3e70e3c69865b1b6b331bc66
aschursina <55243551+aschursina@users.noreply.github.com>
2024-04-13 22:13:55 +0300

    todo result ver1

---
+++ document-structure/todo/task.js
@@ -0,0 +1,24 @@
+let tasks = document.querySelector(".tasks__list");
+let taskInput = document.getElementById("task__input");
+let taskAdd = document.getElementById("tasks__add");
+let taskDelete = document.querySelector(".task__remove");
+
+taskInput.addEventListener("keydown", (event) => {  
+  if(event.key === "Enter") {
+    let newTask = document.createElement("div");
+    newTask.classList.add("task");
+    newTask.innerHTML( 
+      `<div class="task__title">${taskInput.value}</div>
+       <a href="#" class="task__remove">&times;</a>`
+    );
+    tasks.appendChild(newTask);
+    console.log(newTask)
+  }
+})
+
+
+taskDelete.addEventListener("click", (event, newTask) => {
+  event.preventDefault();
+  newTask.remove();
+})
+
