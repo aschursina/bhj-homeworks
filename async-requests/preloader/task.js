5963bec1260d830651231d91e247664002cd6fcd
aschursina <55243551+aschursina@users.noreply.github.com>
2024-04-20 21:56:44 +0300

    task1 result

---
+++ async-requests/preloader/task.js
@@ -0,0 +1,31 @@
+let loader = document.getElementById("loader");
+let items = document.getElementById("items");
+
+
+function createItem(valutes) {
+  items.innerText = '';
+  Object.keys(valutes).forEach((valute) => {
+    items.insertAdjacentHTML("beforeend",  
+    `<div class="item">
+    <div class="item__code">${valutes[valute].CharCode}</div>
+    <div class="item__value">${valutes[valute].Value}</div>
+    <div class="item__currency">руб.</div>
+    </div>`);
+  })
+}
+
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "https://students.netoservices.ru/nestjs-backend/slow-get-courses");
+xhr.send();
+xhr.addEventListener("readystatechange", () => {
+  if(xhr.readyState === xhr.DONE) {
+    let file = JSON.parse(xhr.responseText);
+    valutes = file.response.Valute;
+    createItem(valutes);
+  };
+  
+  if(loader.classList.contains("loader_active")) {
+    loader.classList.remove("loader_active")
+  }
+})
+
