c1c514e5c3cbaa37349fc3c35e65f027b0133147
aschursina <55243551+aschursina@users.noreply.github.com>
2024-04-29 23:01:35 +0300

    task 2 result right

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,6 +1,21 @@
-const textInEditor = document.getElementById("editor");
-const textSaved = textInEditor.value  = localStorage.getItem("text");
+const subModal = document.querySelector(".modal");
+const closedModal = document.querySelector(".modal__close");
 
-textInEditor.addEventListener("input", () => {
-    localStorage.setItem("text", textInEditor.value)
-})
+const getCookie = (name) => {
+  const value = "; " + document.cookie;
+  let parts = value.split(";" + name + "=");
+  if(parts.length === 2) {
+    return parts.pop().split(";").shift();
+  }
+};
+
+closedModal.addEventListener("click", () => {
+  subModal.classList.remove("modal_active");
+  document.cookie = "closedModal=yes";
+});
+  
+
+if(getCookie("closedModal") !== "yes") {
+  subModal.classList.add("modal_active");
+  document.cookie = "closedModal=nope";
+};
